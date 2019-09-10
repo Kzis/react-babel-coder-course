6e75473a40b089a88a7b5bfebe9f332c6d7a47b4
Chalach.mo <driftkao@gmail.com>
2019-09-10 14:59:57 +0700

    policy

--- rest/api/app/articles/controller.js
+++ rest/api/app/articles/controller.js
@@ -1,4 +1,5 @@
 import Articles from './model'
+import ArticlePolicy from './policy'
 
 const ArticlesController = {
     getAll(req, res) {
@@ -11,20 +12,51 @@ const ArticlesController = {
     },
 
     create(req, res) {
-        const article = Articles.create(req.body)
-        res.status(201).json({ article })
+        if (ArticlePolicy.for('create', req.user)) {
+            const article = Articles.create(req.body)
+            res.status(201).json({ article })
+        } else {
+            res.status(401)
+                .json({
+                    article: {
+                        errors: ['You are not allow to create this article']
+                    }
+                })
+        }
+
     },
 
     update(req, res) {
         const id = req.params.id
-        const article = Articles.update(id, req.body)
-        res.status(201).json({ article })
+
+        if (ArticlePolicy.for('update', req.user, Articles.find(id))) {
+            const article = Articles.update(id, req.body)
+            res.status(201).json({ article })
+        } else {
+            res.status(401)
+                .json({
+                    article: {
+                        errors: ['You are not allow to update this article']
+                    }
+                })
+        }
     },
 
     destroy(req, res) {
         const id = req.params.id
-        Articles.destroy(id)
-        res.status(204)
+
+        if (ArticlePolicy.for('destroy', req.user, Articles.find(id))) {
+            Articles.destroy(id)
+            res.status(204)
+        } else {
+            res.status(401)
+                .json({
+                    article: {
+                        errors: ['You are not allow to delete this article']
+                    }
+                })
+        }
+
     },
 
 }
---
+++ rest/api/policy.js
@@ -0,0 +1,9 @@
+
+
+const Policy = {
+    for(method, user, resource) {
+        return this[method](user, resource)
+    }
+}
+
+export default Policy;
