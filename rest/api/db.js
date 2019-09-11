9899beb1d838993f526bab448518789e584f1966
Chalach.mo <driftkao@gmail.com>
2019-09-11 09:50:45 +0700

    update database

--- rest/api/db.js
+++ rest/api/db.js
@@ -1,37 +1,179 @@
 export default {
     categories: [
-        { id: 1, title: "Lang" },
-        { id: 2, title: "Dev" },
-        { id: 3, title: "Editor" },
+        {
+            id: 1,
+            title: 'Languages'
+        },
+        {
+            id: 2,
+            title: 'DevOps'
+        },
+        {
+            id: 3,
+            title: 'Editors'
+        }
     ],
     articles: [
         {
             id: 1,
-            title: "Introduction To Javascript",
-            content: "Javascript content",
+            title: 'Introduction to JavsScript',
+            content: 'Lorem Ipsum',
             authorId: 1,
-            categoryId: 1,
+            categoryId: 1
         },
         {
             id: 2,
-            title: "Introduction To Python",
-            content: "Python content",
+            title: 'Introduction to Python',
+            content: 'Lorem Ipsum',
             authorId: 1,
-            categoryId: 1,
+            categoryId: 1
         },
         {
             id: 3,
-            title: "Introduction To Java",
-            content: "Java content",
+            title: 'Introduction to Elixir',
+            content: 'Lorem Ipsum',
             authorId: 2,
-            categoryId: 2,
+            categoryId: 1
         },
         {
             id: 4,
-            title: "Introduction To Ruby",
-            content: "Ruby content",
+            title: 'Introduction to Ruby',
+            content: 'Lorem Ipsum',
             authorId: 2,
-            categoryId: 3,
+            categoryId: 1
+        },
+        {
+            id: 5,
+            title: 'Introduction to Docket',
+            content: 'Lorem Ipsum',
+            authorId: 1,
+            categoryId: 2
+        },
+        {
+            id: 6,
+            title: 'Introduction to Git',
+            content: 'Lorem Ipsum',
+            authorId: 2,
+            categoryId: 2
+        },
+        {
+            id: 7,
+            title: 'Introduction to Eclipse',
+            content: 'Lorem Ipsum',
+            authorId: 1,
+            categoryId: 3
+        },
+        {
+            id: 8,
+            title: 'Introduction to Visual Studio',
+            content: 'Lorem Ipsum',
+            authorId: 2,
+            categoryId: 3
+        },
+        {
+            id: 9,
+            title: 'Introduction to Netbean',
+            content: 'Lorem Ipsum',
+            authorId: 1,
+            categoryId: 3
+        },
+        {
+            id: 10,
+            title: 'Introduction to APL',
+            content: 'Lorem Ipsum',
+            authorId: 1,
+            categoryId: 1
+        },
+        {
+            id: 11,
+            title: 'Introduction to Ada',
+            content: 'Lorem Ipsum',
+            authorId: 1,
+            categoryId: 1
+        },
+        {
+            id: 12,
+            title: 'Introduction to Basic',
+            content: 'Lorem Ipsum',
+            authorId: 1,
+            categoryId: 1
+        },
+        {
+            id: 13,
+            title: 'Introduction to Boo',
+            content: 'Lorem Ipsum',
+            authorId: 1,
+            categoryId: 1
+        },
+        {
+            id: 14,
+            title: 'Introduction to C/C++',
+            content: 'Lorem Ipsum',
+            authorId: 1,
+            categoryId: 1
+        },
+        {
+            id: 15,
+            title: 'Introduction to Caml',
+            content: 'Lorem Ipsum',
+            authorId: 1,
+            categoryId: 1
+        },
+        {
+            id: 16,
+            title: 'Introduction to Cobol',
+            content: 'Lorem Ipsum',
+            authorId: 1,
+            categoryId: 1
+        },
+        {
+            id: 17,
+            title: 'Introduction to CUDA',
+            content: 'Lorem Ipsum',
+            authorId: 1,
+            categoryId: 1
+        },
+        {
+            id: 18,
+            title: 'Introduction to Dart',
+            content: 'Lorem Ipsum',
+            authorId: 1,
+            categoryId: 1
+        },
+        {
+            id: 19,
+            title: 'Introduction to Elm',
+            content: 'Lorem Ipsum',
+            authorId: 1,
+            categoryId: 1
+        },
+        {
+            id: 20,
+            title: 'Introduction to Eiffel',
+            content: 'Lorem Ipsum',
+            authorId: 1,
+            categoryId: 1
+        },
+        {
+            id: 21,
+            title: 'Introduction to F#',
+            content: 'Lorem Ipsum',
+            authorId: 1,
+            categoryId: 1
+        },
+        {
+            id: 22,
+            title: 'Introduction to Go',
+            content: 'Lorem Ipsum',
+            authorId: 1,
+            categoryId: 1
+        },
+        {
+            id: 23,
+            title: 'Introduction to Kotlin',
+            content: 'Lorem Ipsum',
+            authorId: 1,
+            categoryId: 1
         }
     ],
     users: [
